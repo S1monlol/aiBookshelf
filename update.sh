a56defff9331fd1aa6bfbd405213b1016439349d
S1monlol <simonn@duck.com>
2023-04-24 10:23:52 -0400

    i have stupid

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop aibookshelf
-docker rm aiBookshelf
+docker stop bookshelf
+docker rm bookshelf
 
 git pull
 
-docker build . -t aiBookshelf
-docker run -d --name aibookshelf -p 8080:3000 --env-file .env aibookshelf
+docker build . -t bookshelf
+docker run -d --name bookshelf -p 8080:3000 --env-file .env bookshelf
 
-docker logs -f discord-bot-echo
+docker logs -f bookshelf
