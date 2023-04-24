5009db81e9a75a286d6e283407e12b9a4b4c6bb2
S1monlol <simonn@duck.com>
2023-04-24 08:55:00 -0400

    removed logging spam

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,11 @@ import adapter from '@sveltejs/adapter-node';
 
 export default {
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        vite: {
+            server: {
+                silent: true
+            }
+        }
     }
 };
